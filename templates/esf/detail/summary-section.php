d31f685019ba9d972b6f88817733655ec585b28d
赵 华刚 <zhaohuagang@lifang.com>
2017-03-04 13:59:33 +0800

    yfykh5

---
+++ templates/esf/detail/summary-section.php
@@ -0,0 +1,35 @@
+<div id="summary-section" class="wkzf-panel">
+    <h1>精装修，南北通户型，有钥匙可看房</h1>
+    <div  class="panel-body">
+        <ul class="column-3">
+            <li><dl><dt>总价</dt><dd>240 万</dd></dl></li>
+            <li><dl><dt>户型</dt><dd>2室1厅1卫</dd></dl></li>
+            <li><dl><dt>建筑面积</dt><dd>50m&sup2;</dd></dl></li>
+        </ul>
+        <ul class="esf-tags">
+            <li class="tag selection">精选</li>
+            <li class="tag manwuweiyi">满五唯一</li>
+            <li class="tag subway">地铁</li>
+            <li class="tag school">近学校</li>
+            <li class="tag tongtou">南北通透</li>
+            <li class="tag new">新上</li>            
+        </ul>
+        <hr>
+        <ul class="outline">
+            <li><dl><dt>单价</dt><dd>45000 元 / m&sup2;</dd></dl></li>
+            <li><dl><dt>首付</dt><dd>72万<a class="jisuanqi" href="#"><i class="iconfont icon-jisuanqi"></i></a></dd></dl></li>
+            <li><dl><dt>月供</dt><dd>7988 元</dd></dl></li>
+            <li><dl><dt>年代</dt><dd>2008年</dd></dl></li>
+            <li><dl><dt>类型</dt><dd>公寓</dd></dl></li>
+            <li><dl><dt>楼层</dt><dd>高区／27 层</dd></dl></li>
+            <li><dl><dt>装修</dt><dd>精装修</dd></dl></li>
+            <li><dl><dt>朝向</dt><dd>南北</dd></dl></li>
+        </ul>
+        <hr>
+        <ul class="subway-school">
+            <li class="selectable-flag"><dl><dt>小区</dt><dd>阳光新城</dd></dl></li>
+            <li><dl><dt>地铁</dt><dd>2号线，7号线</dd></dl></li>
+            <li><dl><dt>学校</dt><dd>华师大附小、管弄新村小学、管弄新村小学、管弄新村小学</dd></dl></li>    
+        </ul>
+    </div>
+</div>
