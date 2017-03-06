b14ebf5c21ad73d6bae8396810db65baecb068af
赵 华刚 <zhaohuagang@lifang.com>
2017-03-06 11:50:34 +0800

    yfyk2h5

---
+++ templates/space/index.php
@@ -0,0 +1,13 @@
+<?php require_once("../global.php") ; ?>
+<?php
+$confs = array(
+    "page_title" => "个人店铺首页" , //每个页面标题会在这个字符串后面自动追加 " - 有房有客M站"
+    "keywords" => "" ,
+    "description" => "" ,
+    "match_stylesheet" => true , //是否需要匹配路由的样式表        
+    "match_javascript" => true  //是否需要匹配路由的脚本        
+) ;
+?>
+<?php require_once("../components/head.php") ; ?>            
+
+<?php require_once("../components/footer.php") ; ?>
