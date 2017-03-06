dc7696132edfec8979d0da3250b59c15aface7d4
赵 华刚 <zhaohuagang@lifang.com>
2017-03-06 11:40:41 +0800

    yfyk2h5

--- templates/components/album.php
+++ templates/components/album.php
@@ -1,6 +1,6 @@
 <div id="album-section" class="swiper-container">            
     <div class="swiper-wrapper">                
-        <div class="swiper-slide"><img src="https://imgwater.oss.aliyuncs.com/7288068197b24fd2880b71c156fa86c4.I640_360" class="img-responsive"></div>
+        <div class="swiper-slide"><img src="https://imgwater.oss.aliyuncs.com/7288068197b24fd2880b71c156fa86c4.I640_360" class="img-responsive"><i class="icon-4040 video"></i></div>
         <div class="swiper-slide"><img src="https://imgwater.oss.aliyuncs.com/e1c112deb0014e459232f327429090bc.I640_360" class="img-responsive"></div>
         <div class="swiper-slide"><img src="https://imgwater.oss.aliyuncs.com/d771fa4a38e340878257617fa0553eac.I640_360" class="img-responsive"></div>
         <div class="swiper-slide"><img src="https://imgwater.oss.aliyuncs.com/8693a03c00694604a95c9be4e94dc437.I640_360" class="img-responsive"></div>
