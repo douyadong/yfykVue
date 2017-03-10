847b4a2a35e2600ceef410ff03646328be7769e5
赵 华刚 <zhaohuagang@lifang.com>
2017-03-10 16:14:50 +0800

    yfyk2h5_fe

--- templates/components/download.php
+++ templates/components/download.php
@@ -1,5 +1,5 @@
 <div id="app-download">
-    <span>下载APP</span>
+    <span class="download">下载APP</span>
     <dl>
         <dt><i class="iconfont icon-logotuxing"></i></dt>
         <dd>找对人，买好房</dd>
