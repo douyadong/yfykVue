ef741f0a32901d3006354f647ce8462c47068b52
赵 华刚 <zhaohuagang@lifang.com>
2017-07-06 16:55:13 +0800

    yfyk2h5_fe

--- src/components/common/xfSources.vue
+++ src/components/common/xfSources.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="xf-sources">
-        <dl v-for="(source , index) in items" :key="source.estateId">
-            <dt><img v-lazy="source.imageUrl + '?x-oss-process=image/resize,w_150'" class="img-responsive"></dt>
-            <dd class="title">{{ source.estateName }}</dd>
-            <dd class="summary"> {{source.districtName}} {{source.townName}} {{ source.startSpace }}-{{ source.endSpace }} ㎡ </dd>            
-            <dd class="tags">
-                <span v-if="source.isSoonOpen === '1'">即将开盘</span>
-                <span v-else>在售楼盘</span>
-                <span v-if="source.hasActivity === '1'">有优惠</span>
-                <span v-if="source.isSubwayEstate === '1'">地铁</span>
-                <span v-if="source.hasVideo === '1'">有视频</span>                                       
-            </dd>
-            <dd class="price">{{ source.avgPriceWou }} 元/㎡</dd>
-        </dl>
+        <a :href="getRedirectUrl(source.encryptEstateId)" v-for="(source , index) in items" :key="source.estateId" :data-bigdata="getUvParamsString({ eventName : eventName , estateId : source.estateId , otherParams : otherParams })">
+            <dl>
+                <dt><img v-lazy="source.imageUrl + '?x-oss-process=image/resize,w_150'" class="img-responsive"></dt>
+                <dd class="title">{{ source.estateName }}</dd>
+                <dd class="summary"> {{source.districtName}} {{source.townName}} {{ source.startSpace }}-{{ source.endSpace }} ㎡ </dd>            
+                <dd class="tags">
+                    <span v-if="source.isSoonOpen === '1'">即将开盘</span>
+                    <span v-else>在售楼盘</span>
+                    <span v-if="source.hasActivity === '1'">有优惠</span>
+                    <span v-if="source.isSubwayEstate === '1'">地铁</span>
+                    <span v-if="source.hasVideo === '1'">有视频</span>                                       
+                </dd>
+                <dd class="price">{{ source.avgPriceWou }} 元/㎡</dd>
+            </dl>
+        </a>
     </div>
 </template>
 
@@ -24,12 +26,23 @@ export default {
         
     }
   } ,
-  props : {
-      items : {
-          type : Array ,
-          required : true
+  methods : {
+      getUvParamsString : function({ eventName , estateId , otherParams }) {
+          let eventParam = { new_house_id : estateId } ;
+          if(otherParams !== undefined && otherParams !== null ) {
+              eventParam = Object.assign( eventParam , otherParams ) ;
+          }              
+          return encodeURIComponent(JSON.stringify({ 
+              eventName : eventName , 
+              eventParam : eventParam ,
+              type : 2
+          })) ;
+      } ,
+      getRedirectUrl : function(encryptEstateId) {
+          return "https://m.wkzf.com/shanghai/xfdetail/" + encryptEstateId + ".html" ;
       }
-    } ,
+  } ,
+  props : [ "items" , "eventName" , "otherParams" ] ,
 }
 </script>
 <style lang="less" scoped>
