9c7f1aa40eab09d9bd563e7f6bc6bf13f6fb3d86
tangxuyang <tangxuyang@lifang.com>
2017-07-06 18:44:45 +0800

    clear

--- templates/rent/detail/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <div class="left-tools">
-        <i class="iconfont icon-back" onclick="history.back() ;"></i>
-    </div>
-    <div class="right-tools"></div>
-    <h2>房源详情页</h2>
-</header>
