091e541a0e913d2f337fd96cfada11a037f65303
doudou <douyadong@wkzf.com>
2018-03-08 17:05:41 +0800

    分享出问题，debug一下

--- src/plugins/wechatShare.js
+++ src/plugins/wechatShare.js
@@ -54,7 +54,7 @@ export default {
 	        	success : function(result) {	        		
 	            	var data = result.data ;
 		            wx.config({
-		                debug : false , // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		                debug : true , // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 		                appId : data.appid , // 必填，企业号的唯一标识，此处填写企业号corpid
 		                timestamp : data.timestamp , // 必填，生成签名的时间戳
 		                nonceStr : data.nonce_str , // 必填，生成签名的随机串
@@ -63,6 +63,7 @@ export default {
 		            }) ;
 		            
 		            wx.ready(function(){
+						console.log('config成功');
 		            	wx.onMenuShareTimeline({
 					        title : options.timelineTitle || window.document.title,
 					        /*link : options.linkUrl|| window.location.href,*/
