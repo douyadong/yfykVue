cd488f9ecff0c95df393239b0508c3ae9d983f63
doudou <douyadong@wkzf.com>
2018-03-12 17:38:58 +0800

    fe

--- src/plugins/wechatShare.js
+++ src/plugins/wechatShare.js
@@ -72,9 +72,9 @@ export default {
 							console.log(options.linkUrl);
 							console.log(options);
 		            		wx.onMenuShareTimeline({
-					        	title : options.timelineTitle || window.document.title,
-					        	link : options.linkUrl|| window.location.href,
-					        	imgUrl : options.imgUrl ,
+					        	// title : options.timelineTitle || window.document.title,
+					        	// link : options.linkUrl|| window.location.href,
+					        	imgUrl : "https://cdn.wkzf.com/wkwap_fe/img/source/down/logo.jpg" ,
 					        	//trigger : options.trigger ,
 					        	//success : options.success ,
 					        	//cancel : options.cancel ,
@@ -83,10 +83,10 @@ export default {
 					    	});
 
 					    	wx.onMenuShareAppMessage({
-					        	title : options.title || window.document.title, // 分享标题
+					        	// title : options.title || window.document.title, // 分享标题
 					        	//desc : options.content , // 分享描述
-					        	link : options.linkUrl || window.location.href, // 分享链接
-					        	imgUrl : options.imgUrl , // 分享图标
+					        	// link : options.linkUrl || window.location.href, // 分享链接
+					        	// imgUrl : options.imgUrl  // 分享图标
 					        	//type : '' , // 分享类型,music、video或link，不填默认为link
 					        	//dataUrl : '' , // 如果type是music或video，则要提供数据链接，默认为空
 					        	//trigger : options.trigger ,
