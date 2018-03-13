b8418286d2b371fc4b2d79afb2133a25d7af0c0e
doudou <douyadong@wkzf.com>
2018-03-13 21:19:56 +0800

    分享wechatcode更改

--- src/plugins/wechatShare.js
+++ src/plugins/wechatShare.js
@@ -14,7 +14,7 @@ export default {
     	else if (env === "beta") apiPrefix = "//wechat-beta.wkzf.com/" ;
     	else if (env === "sim") apiPrefix = "//wechat.sim.wkzf/" ;
     	else if (env === "prod") apiPrefix = "//wechat.wkzf.com/" ;
-    	let apiUrl = apiPrefix + "wx_js_sdk_sign.rest?wechatCode=1000021";
+    	let apiUrl = apiPrefix + "wx_js_sdk_sign.rest?wechatCode=1000001";
 
     	let initial = true;
     	wx.ready(function() {
