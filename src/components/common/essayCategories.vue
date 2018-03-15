6972cff7c16c03dc55131ce75b18cc2649dafa11
doudou <douyadong@wkzf.com>
2018-03-15 11:44:19 +0800

    fe

--- src/components/common/essayCategories.vue
+++ src/components/common/essayCategories.vue
@@ -44,89 +44,95 @@
         } ,
         props : [ "activecategoryid" , "activeonetitleid" , "activetwotitleid" ] ,   
         created() { 
-             /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-            获取所有文章栏目
-            -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/ 
-            apiDataFilter.request({
-                apiPath : "learn.category" ,
-                method : "post" ,
-                data : { "cityId" : 43 } ,
-                contentType : "application/json" , 
-                successCallback : res => {
-                    let initData = res.body.data ;
-                    let categoryList = [] ;
-                    let oneTitleList = [] ;
-                    let twoTitleList = [] ;               
-                    /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                    按照以下格式要求整理出所有三个级别类别的文章类别数据
-                    categoryList的格式如下：
-                    [
-                        {"categoryId":22,"oneTitleCount":2,"title":"百科"},
-                        {"categoryId":23,"oneTitleCount":0,"title":"知乎"}
-                    ]
-                    oneTitleList的格式如下：
-                    [
-                        {"categoryId":22,oneTitles:[{"id":123,"title":"购房"},{"id":124,"title":"卖房"}] },
-                        {"categoryId":25,oneTitles:[{"id":125,"title":"装修"},{"id":126,"title":"物业"}] }
-                    ]
-                    twoTitleList的格式如下：
-                    [
-                        {"categoryId":22,"oneTitleId":22,twoTitles:[{"id":123,"title":"购房"},{"id":124,"title":"卖房"}] },
-                        {"categoryId":22,"oneTitleId":25,twoTitles:[{"id":125,"title":"装修"},{"id":126,"title":"物业"}] }
-                    ]
-                    -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/                    
-                    initData && initData.forEach(( category ) => {
-                        let firstSubTitleList = category.firstSubTitleList && category.firstSubTitleList !== undefined ? category.firstSubTitleList : []  ;
+            this.watchRoute();//监听路由变化，动态改变二级标题列表宽度;           
+        },
+        methods :{
+            // 监听路由变化，实时发送请求，动态改变二级菜单宽度;
+            watchRoute(){
+                apiDataFilter.request({
+                    apiPath : "learn.category" ,
+                    method : "post" ,
+                    data : { "cityId" : 43 } ,
+                    contentType : "application/json" , 
+                    successCallback : res => {
+                        let initData = res.body.data ;
+                        let categoryList = [] ;
+                        let oneTitleList = [] ;
+                        let twoTitleList = [] ;               
                         /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                        重新组织categoryList和oneTitleList数据
-                        -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/ 
-                        categoryList.push({
-                            "categoryId" : category.id ,
-                            "oneTitleCount" : firstSubTitleList.length ,
-                            "title" :  category.title
-                        }) ;
-                        oneTitleList.push({
-                            "categoryId" : category.id ,
-                            "oneTitles" : firstSubTitleList
-                        }) ;                        
-                        firstSubTitleList.forEach(( oneTitle ) => {                            
+                        按照以下格式要求整理出所有三个级别类别的文章类别数据
+                        categoryList的格式如下：
+                        [
+                            {"categoryId":22,"oneTitleCount":2,"title":"百科"},
+                            {"categoryId":23,"oneTitleCount":0,"title":"知乎"}
+                        ]
+                        oneTitleList的格式如下：
+                        [
+                            {"categoryId":22,oneTitles:[{"id":123,"title":"购房"},{"id":124,"title":"卖房"}] },
+                            {"categoryId":25,oneTitles:[{"id":125,"title":"装修"},{"id":126,"title":"物业"}] }
+                        ]
+                        twoTitleList的格式如下：
+                        [
+                            {"categoryId":22,"oneTitleId":22,twoTitles:[{"id":123,"title":"购房"},{"id":124,"title":"卖房"}] },
+                            {"categoryId":22,"oneTitleId":25,twoTitles:[{"id":125,"title":"装修"},{"id":126,"title":"物业"}] }
+                        ]
+                        -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/                    
+                        initData && initData.forEach(( category ) => {
+                            let firstSubTitleList = category.firstSubTitleList && category.firstSubTitleList !== undefined ? category.firstSubTitleList : []  ;
                             /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                            重新组织twoTitleList数据
-                            -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
-                            twoTitleList.push({
+                            重新组织categoryList和oneTitleList数据
+                            -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/ 
+                            categoryList.push({
                                 "categoryId" : category.id ,
-                                "oneTitleId" : oneTitle.id ,
-                                "twoTitles" : oneTitle.secondSubTitleList
+                                "oneTitleCount" : firstSubTitleList.length ,
+                                "title" :  category.title
                             }) ;
-                        }) ;
+                            oneTitleList.push({
+                                "categoryId" : category.id ,
+                                "oneTitles" : firstSubTitleList
+                            }) ;                        
+                            firstSubTitleList.forEach(( oneTitle ) => {                            
+                                /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                重新组织twoTitleList数据
+                                -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
+                                twoTitleList.push({
+                                    "categoryId" : category.id ,
+                                    "oneTitleId" : oneTitle.id ,
+                                    "twoTitles" : oneTitle.secondSubTitleList
+                                }) ;
+                            }) ;
                         
-                    }) ;                    
-                    /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                    数据绑定
-                    -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/            
-                    this.apiData.categoryList = categoryList ;
-                    this.apiData.oneTitleList = oneTitleList ;
-                    this.apiData.twoTitleList = twoTitleList ;
-                    /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                    下一次dom节点更新完后执行         
-                    -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
-                    Vue.nextTick(() => {
-                        let categoriesWidth = 0 ;   //第一级类别;
-                        let oneTitleWidth = 0 ;//第二级类别;                   
-                        $(".essay-categories .category div a").each(function() {
-                            let charCount = $(this).text().length ;                           
-                            categoriesWidth += ( charCount * 16 + 30 + 1 ) ; 
-                        }) ; 
-                        $(".essay-categories .oneTitle.visible a").each(function() {
-                            let seedCount = $(this).text().length ;                          
-                            oneTitleWidth += ( seedCount * 14 + 30 + 1 ) ; 
-                        }) ; 
-                        this.pageStates.categoriesWidth = categoriesWidth ;
-                        this.pageStates.oneTitleWidth = oneTitleWidth ;
-                    }) ;                    
+                        }) ;                    
+                        /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                        数据绑定
+                        -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/            
+                        this.apiData.categoryList = categoryList ;
+                        this.apiData.oneTitleList = oneTitleList ;
+                        this.apiData.twoTitleList = twoTitleList ;
+                        /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                        下一次dom节点更新完后执行         
+                        -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
+                        Vue.nextTick(() => {
+                            let categoriesWidth = 0 ;   //第一级类别;
+                            let oneTitleWidth = 0 ;//第二级类别;                   
+                            $(".essay-categories .category div a").each(function() {
+                                let charCount = $(this).text().length ;                           
+                                categoriesWidth += ( charCount * 16 + 30 + 1 ) ; 
+                            }) ; 
+                            $(".essay-categories .oneTitle.visible a").each(function() {
+                                let seedCount = $(this).text().length ;                          
+                                oneTitleWidth += ( seedCount * 14 + 30 + 5 ) ; 
+                            }) ; 
+                            this.pageStates.categoriesWidth = categoriesWidth ;
+                            this.pageStates.oneTitleWidth = oneTitleWidth ;
+                        }) ;                    
                     
-                }
-            }) ;            
+                    }
+                }) ; 
+            }
+        },
+        watch:{
+            "$route":"watchRoute"
         }
     }
 </script>
