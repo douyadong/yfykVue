9b696af492bd0698c2f7c722b36e5f9dc4c283b2
doudou <douyadong@wkzf.com>
2018-03-20 15:22:42 +0800

    测试取经详情页分享链接

--- src/components/learn/detail/share.vue
+++ src/components/learn/detail/share.vue
@@ -281,10 +281,10 @@
                 }                
                 self.$wechatShare({
                   "title" : data.data.articleDetailModel.shareTitle ,
-                  "timelineTitle" : data.data.articleDetailModel.shareTitle ,
-                  "content" : data.data.articleDetailModel.shareContent ,
-                  "imgUrl" : data.data.articleDetailModel.shareImageUrl ,
-                  "linkUrl": 'https://m.wkzf.com/article/app-share.8221.html?cityId=43',
+                  // "timelineTitle" :' data.data.articleDetailModel.shareTitle' ,
+                  // "content" : data.data.articleDetailModel.shareContent ,
+                  "imgUrl" : 'https://imgwater-test.wkzf.com/54e77b8ab28c4996b7f55b642283a992' ,
+                  "linkUrl": "http://"+self.domain+"/learn/detail/hybrid/"+self.articleId+"?agentId="+self.agentId+"&cityId="+self.cityId,
                   "complete":function(){
                     
                   }
